3083a1bb8faa1bf9b582ed5ea959da26b48ed942
Levi Bostian <levi.bostian@gmail.com>
2015-11-19 16:21:29 -0600

    Create basic app that takes stream link and plays it. Allows skip ahead to 5 second point as well.

--- SoundCloudHLSStreamer/ViewController.swift
+++ SoundCloudHLSStreamer/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var streamUrlTextField: UITextField!
+    @IBOutlet weak var streamItButton: UIButton!
+    @IBOutlet weak var jumpAheadButton: UIButton!
+    
+    var avPlayer: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func streamItButtonPressed(sender: UIButton) {
+        self.streamUrlTextField.resignFirstResponder()
+        
+        let streamUrl = NSURL(string: streamUrlTextField.text!)!
+        
+        self.avPlayer = AVPlayer(URL: streamUrl)
+        self.avPlayer?.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions.New, context: nil)
+        self.avPlayer?.rate = 1.0;
+        self.avPlayer?.play()
+        self.avPlayer?.volume = 1.0
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "playerItemDidReachEnd:", name: AVPlayerItemDidPlayToEndTimeNotification, object: avPlayer?.currentItem)
+    }
+    
+    func playerItemDidReachEnd(notification: NSNotification) {
+        NSLog("reached end")
+    }
+
+    @IBAction func jumpAheadButtonPressed(sender: AnyObject) {
+        // Goes to the 5 second mark in the song. 
+        // great info about how this works https://stackoverflow.com/questions/22666190/using-seconds-in-avplayer-seektotime
+        self.avPlayer?.currentItem?.seekToTime(CMTimeMakeWithSeconds(5, 60000))
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if (object != nil && object as? AVPlayer == self.avPlayer! && keyPath != nil && keyPath! == "status") {
+            if (self.avPlayer?.status == AVPlayerStatus.Failed) {
+                NSLog("failed.")
+            } else if (self.avPlayer?.status == AVPlayerStatus.ReadyToPlay) {
+                NSLog("ready to play")
+            } else if (self.avPlayer?.status == AVPlayerStatus.Unknown) {
+                NSLog("unknown")
+            }
+        }
+    }
 
 }
 
